ff58e3a8a6c8a9097598ff725a04ec74556e8455
Aunika Lewis <Aunibonnie321@gmail.comgit config --global user.emailgit config --global user.email Aunibonnie123@gmail.com>
2024-10-30 16:56:55 -0500

    completed adding monthly income to table graph had tutor help from Andrew thank you

--- server/public/client.js
+++ server/public/client.js
@@ -1,4 +1,5 @@
 console.log( 'js' );
+
 function addSalary(){
     console.log( 'in addSalary' );
     let objectToSend ={
@@ -8,6 +9,7 @@ function addSalary(){
         title: document.getElementById( 'titleInput' ).value,
         annualSalary: document.getElementById( 'annualSalaryInput' ).value,
     }
+    // monthlySalary = monthlySalary + Number (document.getElementById( 'annualSalaryInput' ).value);
     document.getElementById( 'firstNameInput' ).value ='';
     document.getElementById( 'lastNameInput' ).value ='';
     document.getElementById( 'idInput' ).value ='';
@@ -22,14 +24,37 @@ function addSalary(){
 
 function getSalary(){
     axios.get( '/salary' ).then( function (response ){
+        let annualSalary = 0;
         console.log( response.data );
+        // const monthlySalary = document.getElementById( 'annualSalaryInput').value 
         const el = document.getElementById( 'salaryTable' );
         el.innerHTML='';
         for( let i=0; i<response.data.length; i++){
+            console.log(annualSalary);
             const thisSalary = response.data[i];
-            el.innerHTML += `<table><tr><td>${thisSalary.firstName}<td>${ thisSalary.lastName }<td>${ thisSalary.id}<td>${ thisSalary.title }<td>${ thisSalary.annualSalary }</td></td></td></td></td></tr></table>`
+            annualSalary = annualSalary + Number(thisSalary.annualSalary)
+            el.innerHTML += `<tr><td>${thisSalary.firstName}<td>${ thisSalary.lastName }<td>${ thisSalary.id}<td>${ thisSalary.title }<td>${ thisSalary.annualSalary }</td></td></td></td></td></tr>`
         }
+
+        console.log("Monthly Salary:", annualSalary);
+        document.getElementById('monthlyIncome').innerHTML = annualSalary / 12;
     })
 }
 
 getSalary();
+
+// function calculateMonthlySalary(annualSalary) {
+//     return annualSalary / 12;
+//   }
+  
+//   // Example usage:
+//   let annualSalary = 60000;
+//   let monthlySalary = calculateMonthlySalary(annualSalary);
+  
+//   console.log("Monthly Salary:", monthlySalary);
+
+// function calculateIncome() {
+// 	var monthlyIncome = dailyIncome * 22;
+
+// 	document.getElementById("monthly-income").textContent = monthlyIncome.toFixed(2);
+// }
